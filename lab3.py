f3c8047e0ce1eb04a50b6de61375d4fe1b2badad
KASYMZHOMARTT <166055601+KASYMZHOMARTT@users.noreply.github.com>
2024-09-28 11:13:59 +0600

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,34 @@
+"""x=int(input("Б"))
+y=int(input("М"))
+
+z=y//x
+
+print(z)
+z=y%x
+
+print(z)"""
+
+"""x=int(input("A "))
+
+y=x//1000
+
+print("The digit in the thousands position is ", y)
+
+z=(x%1000)//100
+
+print("The number in the hundreds position is ", z)
+
+m=((x%1000)%100)//10
+
+print("The digit in the tens position is ", m)
+
+n=(((x%1000)%100)%10)
+
+print("The digit in the position of units ", n)"""
+
+"""x = int(input("х"))
+
+y = (x - 1) // 2 + 1
+
+print(y)"""
+
