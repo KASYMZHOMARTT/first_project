8364e805f0a94e1f933c1457cf28a9a9870ae9d0
KASYMZHOMARTT <166055601+KASYMZHOMARTT@users.noreply.github.com>
2024-10-26 16:45:30 +0600

    Add files via upload

---
+++ lab_8.py
@@ -0,0 +1,40 @@
+# 1.1
+# user_input = input("text: ")
+# char_list = list(user_input.lower())
+# print( char_list)
+
+# 1.2
+# user_list = input("text: ")
+# vowels = "aeiou"
+#
+# list_vow = []
+# list_cons = []
+# list_sym = []
+#
+# for i in user_list:
+#
+#     if i in vowels:
+#         list_vow.append(i)
+#     elif i.isalpha():
+#         list_cons.append(i)
+#     else:
+#         list_sym.append(i)
+#
+# print("list_vow:", list_vow)
+# print("list_cons:", list_cons)
+# print("list_sym:", list_sym)
+
+ # 2.1
+# name = input('Student name - ')
+# ass = input('scores for assignments = ')
+# tests = input('tests = ')
+# labs = input('labs = ')
+# student = {
+#     'name':name,
+#     'assignment':ass,
+#     'test':tests,
+#     'lab':labs,
+# }
+# print(student)
+
+# 2.2
